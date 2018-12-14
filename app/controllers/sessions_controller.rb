8c20f1dfe2a3d973d6abc49c222a625e28262b7e
Mironchikov Ilya <i.mironchikov@gmail.com>
2018-12-15 00:01:16 +0300

    login system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
-  def index; end
-
   # @return [Object]
-  def new; end
+  def login; end
+
+  def authenticate
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'chat_room#index'
-  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#login'
+  post 'login', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
