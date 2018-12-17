38ef1b88497292e5d53ebbf0c40a09c28b75f7ff
Mironchikov Ilya <i.mironchikov@gmail.com>
2018-12-17 15:45:02 +0300

    start auth system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,24 @@ class SessionsController < ApplicationController
   def login; end
 
   def authenticate
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'There was something wrong...'
+      render 'login'
+    end
   end
 
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have logged out!'
+    redirect_to login_path
+  end
 
+  def signup
+    @user = User.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'chat_room#index'
+  get 'signup', to: 'sessions#signup'
   get 'login', to: 'sessions#login'
-  post 'login', to: 'sessions#create'
+  post 'login', to: 'sessions#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
