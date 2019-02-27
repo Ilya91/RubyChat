4825024a2ec1fe5f763f0415d64e492e04bc00a5
Mironchikov Ilya <i.mironchikov@gmail.com>
2019-02-27 17:05:32 +0300

    associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_135112) do
+ActiveRecord::Schema.define(version: 2019_02_27_133354) do
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 2019_02_26_135112) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "trains", force: :cascade do |t|
